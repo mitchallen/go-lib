919668e23c0d807077016bb2e7c1d6bc181c4f46
Mitch Allen <github@mitchallen.com>
2022-03-21 23:30:42 -0400

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mitchallen/go-lib
+
+go 1.18
---
+++ init.go
@@ -0,0 +1,15 @@
+/**
+ * Author: Mitch Allen
+ * File: init.go
+ */
+
+package lib
+
+import (
+	"fmt"
+)
+
+// Seed rand
+func init() {
+	fmt.Println("[go-lib] initializing ...")
+}
